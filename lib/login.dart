b2b2c30845abc15da6ca57d569a79b43fc3b618c
ouisaaa_pc <ouisaaa@gmail.com>
2023-04-08 20:55:56 +0900

    04.08 로그인 화면 가운데 정렬

--- lib/login.dart
+++ lib/login.dart
@@ -5,25 +5,17 @@ void main() {
 }
 class LoginPage extends StatelessWidget {
 
-  bool enabled = false;
-  String stateText = "disable";
-
-  void changeCheck() {
-    if (enabled) {
-      stateText = "disable";
-      enabled = false;
-    } else {
-      stateText = "enable";
-      enabled = true;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar:AppBar( title: Text("Stateless Test"),),
-          body: TestScreen()
+          appBar:AppBar( title: Text("로그인"),),
+          body: Center(
+             child: Column(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 children:[ TestScreen(),]
+             ),
+          )
         )
     );
   }
@@ -41,63 +33,80 @@ class MyFormState extends State<TestScreen>{
 
     @override
     Widget build(BuildContext context){
-      return Column(
-        children: [
-          Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'ID'
+      return SizedBox(
+        width: 500,
+        height: 500,
+        child: Column(
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'ID'
+                    ),
+                    validator: (value){
+                      if(value?.isEmpty ?? false){
+                        return '아이디를 입력하시오';
+                      }
+                      return null;
+                    },
+                    onSaved: (String? value){
+                      id = value;
+                    },
                   ),
-                  validator: (value){
-                    if(value?.isEmpty ?? false){
-                      return '아이디를 입력하시오';
-                    }
-                    return null;
-                  },
-                  onSaved: (String? value){
-                    id = value;
-                  },
-                ),
-                TextFormField(
-
-                  decoration: InputDecoration(
-                      labelText: 'Password'
-                  ),
-                  validator: (value){
-                    if(value?.isEmpty ?? false){
-                      return '아이디를 입력하시오';
-                    }
-                    return null;
-                  },
-                  onSaved: (String? value){
-                    password = value;
-                  },
-                )
-              ],
-            ),
-          ),
-          ElevatedButton(onPressed: (){
-            if(_formKey.currentState?.validate() ?? false){
-              _formKey.currentState?.save();
-              print('ID: $id, Password: $password');
-            }
-          }, child: Text('login')),
-          ElevatedButton(onPressed: (){
-            //테스트용 알림창 코드
-            showDialog(context: context,barrierDismissible: false, builder: (BuildContext Context){
-              return AlertDialog(
-                title: Text("Dialog title"),
-                content: Text("회원가입 클릭"),
-                actions: [
-                  TextButton(onPressed: (){Navigator.of(context).pop();}, child: Text("OK"))
+                  SizedBox(height: 10,),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Password'
+                    ),
+                    validator: (value){
+                      if(value?.isEmpty ?? false){
+                        return '아이디를 입력하시오';
+                      }
+                      return null;
+                    },
+                    onSaved: (String? value){
+                      password = value;
+                    },
+                  )
                 ],
-              );
-            });//테스트용 알림창 코드
-          }, child: Text('회원가입')),
-        ],
+              ),
+            ),
+            SizedBox(height: 10,),
+            ElevatedButton(onPressed: (){
+              if(_formKey.currentState?.validate() ?? false){
+                _formKey.currentState?.save();
+                print('ID: $id, Password: $password');
+              }
+            }, child: Text('login'),
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                  minimumSize: Size(500,50)
+              ),
+
+            ),
+            SizedBox(height: 10,),
+            ElevatedButton(onPressed: (){
+              //테스트용 알림창 코드
+              showDialog(context: context,barrierDismissible: false, builder: (BuildContext Context){
+                return AlertDialog(
+                  title: Text("Dialog title"),
+                  content: Text("회원가입 클릭"),
+                  actions: [
+                    TextButton(onPressed: (){Navigator.of(context).pop();}, child: Text("OK"))
+                  ],
+                );
+              });//테스트용 알림창 코드
+            }, child: Text('회원가입'),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                minimumSize: Size(500,50)
+            ),),
+          ],
+        ),
       );
+
     }
 }
